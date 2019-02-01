bb332ccb3784e2fe682f657675154d372a2084cf
Prabu Arumugam <Prabu.Arumugam@Honeywell.com>
2019-01-31 22:04:50 -0500

    Implemented Selection Sort.

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -2,5 +2,24 @@
 
 module.exports = {
     sort: function (a) {
+        for (let i = 0; i < a.length; i += 1) {
+            let min = a[i]; // Assume current element as minimum
+            let minIndex = i;
+
+            // Find minimum (smallest) element from remaining array
+            for (let j = i + 1; j < a.length; j += 1) {
+                if (a[j] < min) {
+                    min = a[j];
+                    minIndex = j;
+                }
+            }
+
+            // Swap i'th element with minimum element
+            if (i != minIndex) {
+                let temp = a[i];
+                a[i] = a[minIndex];
+                a[minIndex] = temp;
+            }
+        }
     }
 };
--- sorting/test-sorting.js
+++ sorting/test-sorting.js
@@ -3,10 +3,12 @@
 var bubbleSort = require('./bubbleSort');
 var mergeSort = require('./mergeSort');
 var insertionSort = require('./insertionSort');
+var selectionSort = require('./selectionSort');
 
 var a = [5, 7, 1, -2, 8, 5, 0];
 console.log('Before: ' + a);
 // bubbleSort.sort(a);
-// mergeSort.sort(a);
-insertionSort.sort(a);
+mergeSort.sort(a);
+// insertionSort.sort(a);
+// selectionSort.sort(a);
 console.log('After: ' + a);
