6e48930de3db4b268ef5d6ffe99d8a4252630e9a
Prabu Arumugam <Prabu.Arumugam@Honeywell.com>
2019-02-05 22:24:08 -0500

    Added water-jugs problem.

--- array/test-array.js
+++ array/test-array.js
@@ -1,10 +1,23 @@
 // test-array.js
 
 var moveZeroes = require('./moveZeroes');
+var waterJugs = require('./waterJugs');
 
-// let a = [1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6];
-let a = [1, 0, 0, 0, 3, 0, 0, 0, 5, 0, 6];
-console.log('Before: ' + a);
-// moveZeroes.moveZeroesNoOrder(a);
-moveZeroes.moveZeroesWithOrder(a);
-console.log('Result: ' + a);
+// testMoveZeroes();
+testWaterJugs();
+
+function testMoveZeroes() {
+    // let a = [1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6];
+    let a = [1, 0, 0, 0, 3, 0, 0, 0, 5, 0, 6];
+    console.log('Before: ' + a);
+    // moveZeroes.moveZeroesNoOrder(a);
+    moveZeroes.moveZeroesWithOrder(a);
+    console.log('Result: ' + a);
+}
+
+function testWaterJugs() {
+    let r = [3, 2, 4, 7, 5, 1, 6];
+    let b = [4, 2, 7, 1, 3, 6, 5];
+    let pairs = waterJugs.waterJugsLinearSearch(r, b);
+    console.log('Pairs:\n' + pairs.join('\n'));
+}
---
+++ array/waterJugs.js
@@ -0,0 +1,31 @@
+// waterJugs.js
+
+module.exports = {
+    /*
+        Problem Description:
+        Suppose that you are given n red and n blue water jugs, all of
+        different shapes and sizes. All red jugs hold different amounts
+        of water, as do the blue ones. There is a blug jug that holds
+        the same amount of water for every red jug. Find a grouping of
+        jugs into pairs of red and blue jugs that hold the same amount
+        of water, in such a way that minimum number of comparisons are
+        made to determine the grouping.
+    */
+
+    waterJugsLinearSearch: function (r, b) {
+        let pairs = [];
+
+        for (let i = 0; i < r.length; i += 1) {
+            let pair = [i];
+            for (let j = 0; j < b.length; j += 1) {
+                if (b[j] == r[i]) {
+                    pair.push(j);
+                    break;
+                }
+            }
+            pairs.push(pair);
+        }
+
+        return pairs;
+    }
+};
