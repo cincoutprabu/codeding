2c215bda446cf5cf142cc3fd5a8eac224064e70c
Prabu Arumugam <Prabu.Arumugam@Honeywell.com>
2019-06-06 11:44:49 -0400

    Added problem statement for SherlockAndValidString problem.

--- HackerRank/SherlockAndValidString.cs
+++ HackerRank/SherlockAndValidString.cs
@@ -14,6 +14,21 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
+/*
+    Solution for Sherlock and Valid String coding challenge.
+
+    Sherlock considers a string to be valid if all characters of the string
+    appear the same number of times. It is also valid if he can remove just
+    1 character at 1 index in the string, and the remaining characters will
+    occur the same number of times. Given a string s, determine if it is
+    valid. If so, return YES, otherwise return NO.
+
+    For example, if s = abc, it is a valid string because frequencies are 
+    { a: 1, b: 1, c: 1 }. So is s = abcc because we can remove one c and have
+    1 of each character in the remaining string. If s = abccc however, the
+    string is not valid as we can only remove 1 occurrence of c. That would
+    leave character frequencies of { a: 1, b: 1, c: 2 }.
+ */
 class Solution {
 
     static string isValid(string s) {
